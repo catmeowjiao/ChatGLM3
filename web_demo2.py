61723201496807e7caa9414c1d76a39467b2cfa3
meowjiao <138079152+catmeowjiao@users.noreply.github.com>
2023-11-03 21:05:20 +0800

    Update web_demo2.py

--- web_demo2.py
+++ web_demo2.py
@@ -15,7 +15,7 @@ model_path = "THUDM/chatglm3-6b"
 @st.cache_resource
 def get_model():
     tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-    model = AutoModel.from_pretrained(model_path, trust_remote_code=True).cuda()
+    model = AutoModel.from_pretrained(model_path, trust_remote_code=True).float()
     # 多显卡支持,使用下面两行代替上面一行,将num_gpus改为你实际的显卡数量
     # from utils import load_model_on_gpus
     # model = load_model_on_gpus("THUDM/chatglm3-6b", num_gpus=2)
